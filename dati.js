d9f0c8fdcc0223cc8f4116a8c42e312091e2b91b
Fabio D Filippone <152060236+fabiofilippone@users.noreply.github.com>
2025-06-27 16:52:22 +0200

    Create dati.js

---
+++ dati.js
@@ -0,0 +1,11 @@
+// dati.js
+const orariMesse = [
+  { giorno: "Domenica", ora: "10:00" },
+  { giorno: "Mercoledì", ora: "18:30" },
+  { giorno: "Venerdì", ora: "20:00" },
+];
+
+const contatti = {
+  telefono: "0123 456789",
+  email: "info@parrocchia.it"
+};
