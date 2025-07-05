4ff092391879e0ae1001aebd6bbbfcde09991d57
Fabio Filippone <fabio.filippone@gmail.com>
2025-07-05 13:53:22 +0200

    Aggiunti data10.js e t10.html

---
+++ data10.js
@@ -0,0 +1,65 @@
+  const datia = [
+       { atleta: "Tommaso MARINI",        flag: "it", rank: "9995" , value: ["NN", "V5", "V5", "V5", "V5", "V5", "V5",30,10], club: "RMAPP" },
+       { atleta: "Vladislav MYLNIKOV",    flag: "tr", rank: "N/A"  , value: ["D3", "NN", "V5", "V5", "V5", "V5", "V5",28,10], club: "RMAPP" },
+       { atleta: "Oscar GEUDVERT",        flag: "lb", rank: ""     , value: ["D2", "D1", "NN", "V5", "V5", "D4", "V5",22,16], club: "ANJES" },
+       { atleta: "Alex Sheen AZIZIAN",    flag: "us", rank: "9996" , value: ["D1", "D0", "D0", "NN", "D1", "D1", "D0", 3,30], club: "ANJES" },
+       { atleta: "Mustafa Burak CUFADAR", flag: "il", rank: "9997" , value: ["D2", "D0", "D0", "V5", "NN", "D4", "D3",14,26], club: "RMCC"  },
+       { atleta: "David SOSNOV",          flag: "jp", rank: "9998" , value: ["D1", "D1", "V5", "V5", "V5", "NN", "D3",20,24], club: "TOCH4" },
+       { atleta: "Henri ASK",             flag: "ph", rank: "9999" , value: ["D1", "D3", "D1", "V5", "V5", "V5", "NN",20,21], club: "LTGSS" }
+  ];
+
+  const datib = [
+       { atleta: "Tommaso MARINI",        flag: "it", rank: "9995" , value: ["NN", "V5", "V5", "V5", "V5", "V5",25,10], club: "RMAPP" },
+       { atleta: "Vladislav MYLNIKOV",    flag: "tr", rank: "N/A"  , value: ["D3", "NN", "V5", "V5", "V5", "V5",23,10], club: "RMAPP" },
+       { atleta: "Oscar GEUDVERT",        flag: "lb", rank: ""     , value: ["D2", "D1", "NN", "V5", "V5", "D4",17,16], club: "ANJES" },
+       { atleta: "Alex Sheen AZIZIAN",    flag: "us", rank: "9996" , value: ["D1", "D0", "D0", "NN", "D1", "D1", 3,25], club: "ANJES" },
+       { atleta: "Mustafa Burak CUFADAR", flag: "il", rank: "9997" , value: ["D2", "D0", "D0", "V5", "NN", "D4",14,21], club: "RMCC"  },
+       { atleta: "David SOSNOV",          flag: "jp", rank: "9998" , value: ["D1", "D1", "V5", "V5", "V5", "NN",20,19], club: "TOCH4" }
+  ];
+
+
+  const datic = [
+       { atleta: "Tommaso MARINI",        flag: "it", rank: "9995" , value: ["NN", "V5", "V5", "V5", "V5", 25,10], club: "RMAPP" },
+       { atleta: "Vladislav MYLNIKOV",    flag: "tr", rank: "N/A"  , value: ["D3", "NN", "V5", "V5", "V5", 23,10], club: "RMAPP" },
+       { atleta: "Francesco Karol FILIPPONE",        flag: "lb", rank: ""     , value: ["D2", "D1", "NN", "V5", "V5", 17,16], club: "ANJES" },
+       { atleta: "Alex Sheen AZIZIAN",    flag: "us", rank: "9996" , value: ["D1", "D0", "D0", "NN", "D1",  3,25], club: "ANJES" },
+       { atleta: "Mustafa Burak CUFADAR", flag: "il", rank: "9997" , value: ["D2", "D0", "D0", "V5", "NN", 14,21], club: "RMCC"  }
+  ];
+
+  const datid = [
+       { atleta: "Tommaso MARINI",        flag: "it", rank: "9995" , value: ["NN", "V5", "V5", "V5", 25,10], club: "RMAPP" },
+       { atleta: "Vladislav MYLNIKOV",    flag: "tr", rank: "N/A"  , value: ["D3", "NN", "V5", "V5", 23,10], club: "RMAPP" },
+       { atleta: "Oscar GEUDVERT",        flag: "lb", rank: ""     , value: ["D2", "D1", "NN", "V5", 17,16], club: "ANJES" },
+       { atleta: "Alex Sheen AZIZIAN",    flag: "us", rank: "9996" , value: ["D1", "D0", "D0", "NN",  3,25], club: "ANJES" }
+  ];
+
+  const datie = [
+       { atleta: "Tommaso MARINI",        flag: "it", rank: "9995" , value: ["NN", "V5", "V5", 25,10], club: "RMAPP" },
+       { atleta: "Vladislav MYLNIKOV",    flag: "tr", rank: "N/A"  , value: ["D3", "NN", "V5", 23,10], club: "RMAPP" },
+       { atleta: "Oscar GEUDVERT",        flag: "lb", rank: ""     , value: ["D2", "D1", "NN", 17,16], club: "ANJES" }
+  ];
+
+
+  const detaila = [
+      ["CHALDAIOU Maria-Eleni",  "MORANDI Carlotta",      "D1", "V5"],
+      ["JELINSKA Martyna",       "SHAFAROSTAVA Varvara",  "V5", "D3"],
+      ["KAGAN TYAGUNOV May",     "TSANG Amelie",          "D0", "V5"],
+      ["CHALDAIOU Maria-Eleni",  "CRISTINO Anna",         "D1", "V5"],
+      ["JELINSKA Martyna",       "MORANDI Carlotta",      "V4", "D3"],
+      ["SHAFAROSTAVA Varvara",   "TSANG Amelie",          "D0", "V5"],
+      ["KAGAN TYAGUNOV May",     "CRISTINO Anna",         "D1", "V5"],
+      ["JELINSKA Martyna",       "CHALDAIOU Maria-Eleni", "D3", "V5"],
+      ["MORANDI Carlotta",       "TSANG Amelie",          "D2", "V5"],
+      ["KAGAN TYAGUNOV May",     "SHAFAROSTAVA Varvara",  "D2", "V5"],
+      ["JELINSKA Martyna",       "CRISTINO Anna",         "D0", "V5"],
+      ["CHALDAIOU Maria-Eleni",  "TSANG Amelie",          "V5", "D2"],
+      ["KAGAN TYAGUNOV May",     "MORANDI Carlotta",      "D1", "V5"],
+      ["CRISTINO Anna",          "SHAFAROSTAVA Varvara",  "V5", "D1"],
+      ["JELINSKA Martyna",       "TSANG Amelie",          "V5", "D2"],
+      ["CHALDAIOU Maria-Eleni",  "KAGAN TYAGUNOV May",    "V5", "D4"],
+      ["SHAFAROSTAVA Varvara",   "MORANDI Carlotta",      "D2", "V5"],
+      ["CRISTINO Anna",          "TSANG Amelie",          "V5", "D4"],
+      ["KAGAN TYAGUNOV May",     "JELINSKA Martyna",      "D4", "V5"],
+      ["CHALDAIOU Maria-Eleni",  "SHAFAROSTAVA Varvara",  "V5", "D0"],
+      ["MORANDI Carlotta",       "CRISTINO Anna",         "D3", "V5"]
+  ];
